c46f00c1dd86b6c1079922b5f34f2c727ac54d8f
amitdavidi <amitdavidi12@gmail.com>
2022-12-18 12:11:28 +0200

    all done

--- main.c
+++ main.c
@@ -21,13 +21,13 @@ typedef struct program_data {
     int num_counters;
     int log_enable;
     char files_arr[MAX_NUM_COUNTERS][MAX_FILE_NAME_LEN];
-    
+
     /* closed at the end of runtime */
     FILE *log_files_arr[MAX_NUM_THREADS];
-    
+
     /* destroyed at the end of runtime */
     pthread_mutex_t counter_mutex_arr[MAX_NUM_COUNTERS];
-    
+
     /* destroyed at the end of runtime*/
     pthread_t theards_arr[MAX_NUM_THREADS];
 
@@ -62,12 +62,10 @@ typedef struct stats {
     double average_turnaround;
 } Stats;
 
-
 Stats program_stats;
 Program_Data *program_data;
 Queue *JobQueue;
-
-
+long long int getCurrentTime();
 long long int readNumFromCounter(int counter_number);
 void increment(int counter_number);
 void decrement(int counter_number);
@@ -83,9 +81,9 @@ void sleep_ms(int ms);
 void executeWorkerJob(Job *job);
 void executeDispatcherJob(Job *job);
 void handleJob(Job *job);
-bool initCounterMutexs();
-int initThreadsArr();
-int initCounters(void);
+void initCounterMutexs();
+void initThreadsArr();
+void initCounters();
 bool isEmpty();
 void Enqueue(Job *job);
 Job *Dequeue();
@@ -194,12 +192,9 @@ Job *createJob(char *line)
     Job *job = (Job *)malloc(sizeof(Job));
     job->line_copy = strdup(line);
     job->next_job = NULL;
-    
-    /* get the time of job submission */
-    struct timespec ts;
-    timespec_get(&ts, TIME_UTC);
-    job->submission_time = (long long int)nano_to_ms(ts.tv_nsec) + ts.tv_sec * 1000;
 
+    /* get the time of job submission */
+    job->submission_time = getCurrentTime();
 
     remove_new_line_char(line);
     int num_of_commands = 0;
@@ -222,14 +217,9 @@ Job *createJob(char *line)
  */
 void waitPendingJobs()
 {
-    printf("Dispatcher waiting untill all jobs are done...\n");
-
     pthread_mutex_lock(&(JobQueue->queue_mutex));
-
     pthread_cond_wait(&(JobQueue->all_work_done), &(JobQueue->queue_mutex));
-
     pthread_mutex_unlock(&(JobQueue->queue_mutex));
-    printf("all work done, dispatcher waking up..\n");
 }
 
 void executeWorkerJob(Job *job)
@@ -255,10 +245,9 @@ void executeWorkerJob(Job *job)
             else if (strncmp(job->commands_to_execute[i], "increment", strlen("increment")) == 0) {
 
                 file_number = atoi(job->commands_to_execute[i] + strlen("increment"));
+
                 pthread_mutex_lock(&(program_data->counter_mutex_arr[file_number]));
-
                 increment(file_number);
-
                 pthread_mutex_unlock(&(program_data->counter_mutex_arr[file_number]));
 
             }
@@ -266,10 +255,9 @@ void executeWorkerJob(Job *job)
             else if (strncmp(job->commands_to_execute[i], "decrement", strlen("decrement")) == 0) {
 
                 file_number = atoi(job->commands_to_execute[i] + strlen("decrement"));
+
                 pthread_mutex_lock(&(program_data->counter_mutex_arr[file_number]));
-
                 decrement(file_number);
-
                 pthread_mutex_unlock(&(program_data->counter_mutex_arr[file_number]));
             }
         }
@@ -286,12 +274,7 @@ void executeDispatcherJob(Job *job)
     else if (strncmp(job->commands_to_execute[0], "dispatcher_msleep", strlen("dispatcher_msleep")) == 0) {
 
         int sleep_val = atoi(job->commands_to_execute[0] + strlen("dispatcher_msleep"));
-
-        printf("Dispatcher sleepin' for: %d\n\n", sleep_val);
-
         sleep_ms(sleep_val);
-
-        printf("Dispatcher done sleeping");
     }
 }
 
@@ -348,57 +331,48 @@ void *workerThreadFunction(void *arg)
         pthread_mutex_lock(&(JobQueue->queue_mutex));
 
         while (isEmpty()) {
-            printf("Queue empty, thread is sleeping...\n");
-
             program_data->num_of_sleeping_threads++;
 
             /* "signal" the main thread that this thread is sleeping.*/
-            printf("%d threads are sleeping out of %d total threads...\n", program_data->num_of_sleeping_threads, program_data->num_threads);
-
             if (program_data->num_of_sleeping_threads == program_data->num_threads) {
-                printf("all Jobs done\n.");
                 pthread_cond_signal(&(JobQueue->all_work_done));
             }
 
             pthread_cond_wait(&(JobQueue->queue_not_empty_cond_var), &(JobQueue->queue_mutex));
             program_data->num_of_sleeping_threads--;
-
-            printf("Thread woke up, queue has jobs to do!\n");
         }
 
         job = Dequeue();
         pthread_mutex_unlock(&(JobQueue->queue_mutex));
 
-
-
-
-        timespec_get(&ts, TIME_UTC); // start time of job
+        timespec_get(&ts, TIME_UTC);  // start time of job
         if (program_data->log_enable) {
-            fprintf(program_data->log_files_arr[id], "TIME %lld: START job %s", (long long int)nano_to_ms(ts.tv_nsec) + ts.tv_sec * 1000 - program_stats.start_time,
+            fprintf(program_data->log_files_arr[id], "TIME %lld: START job %s",
+                    getCurrentTime() - program_stats.start_time,
                     job->line_copy);
         }
 
         executeWorkerJob(job);
 
-
-        timespec_get(&ts, TIME_UTC); // end time of job
+        timespec_get(&ts, TIME_UTC);  // end time of job
         if (program_data->log_enable) {
             // write end time
-            fprintf(program_data->log_files_arr[id], "TIME %lld: END job %s", (long long int)nano_to_ms(ts.tv_nsec) + ts.tv_sec * 1000  - program_stats.start_time, job->line_copy);
+            fprintf(program_data->log_files_arr[id], "TIME %lld: END job %s",
+                    getCurrentTime() - program_stats.start_time,
+                    job->line_copy);
         }
 
+        /* CALCULATE STATS */
+        long long int turnaround_time = getCurrentTime() - job->submission_time;
 
-        /* CALCULATE STATS */    
-        long long int turnaround_time = nano_to_ms(ts.tv_nsec) + ts.tv_sec * 1000  - job->submission_time;
-        
         program_stats.total_jobs++;
         program_stats.total_turnaround += turnaround_time;
 
-        if(turnaround_time > program_stats.max_turnaround) {
+        if (turnaround_time > program_stats.max_turnaround) {
             program_stats.max_turnaround = turnaround_time;
         }
 
-        if(turnaround_time < program_stats.min_turnaround || program_stats.min_turnaround == -1) {
+        if (turnaround_time < program_stats.min_turnaround || program_stats.min_turnaround == -1) {
             program_stats.min_turnaround = turnaround_time;
         }
 
@@ -406,24 +380,22 @@ void *workerThreadFunction(void *arg)
     }
 }
 
-bool initCounterMutexs()
+void initCounterMutexs()
 {
     int i;
     for (i = 0; i < program_data->num_counters; i++) {
 
         if (pthread_mutex_init(&(program_data->counter_mutex_arr[i]), NULL) != MUTEX_INIT_SUCESS) {
             fprintf(stderr, "pthread_mutex_init #%d has failed\n", i);
-            return false;
+            exit(EXIT_FAILURE);
         }
     }
-
-    return true;
 }
 
 /*
  * This function creates the worker threads.
  */
-int initThreadsArr()
+void initThreadsArr()
 {
     char file_name_buffer[MAX_FILE_NAME_LEN] = {0};
 
@@ -433,43 +405,44 @@ int initThreadsArr()
         *arg = i;
         if (pthread_create(&program_data->theards_arr[i], NULL, &workerThreadFunction, arg) != 0) {
             perror("Failed to create thread");
-            return 1;
+            exit(EXIT_FAILURE);
         }
 
+        /* Create log file for thread #<i> */
         if (program_data->log_enable) {
             sprintf(file_name_buffer, "thread%.2d.txt", i);  // create name
             program_data->log_files_arr[i] = fopen(file_name_buffer, "w+");
         }
     }
-
-    return 0;
 }
 
 /*
  * This function open counter files.
  */
-int initCounters(void)
+void initCounters()
 {
-    int i;
     char file_name_buffer[MAX_FILE_NAME_LEN] = {0};
+    FILE *fp;
 
-    for (i = 0; i < program_data->num_counters; i++) {
-
-        sprintf(file_name_buffer, "counter%.2d.txt", i);  // create name
-
+    for (int i = 0; i < program_data->num_counters; i++) {
+        sprintf(file_name_buffer, "counter%.2d.txt", i);       // create name
         strcpy(program_data->files_arr[i], file_name_buffer);  // store the name
 
-        FILE *fp = fopen(program_data->files_arr[i], "w+");
-        fprintf(fp, "0");  // write 0 to file
+        /* create and write 0*/
+        fp = fopen(program_data->files_arr[i], "w+");
+        if (!fp) {
+            fprintf(stderr, "Error creating file %s\n", file_name_buffer);
+            exit(EXIT_FAILURE);
+        }
 
+        fprintf(fp, "0");  // write 0 to file
         fclose(fp);
     }
-
-    return 0;
 }
 
-
-void initProgramStats() {
+/* initialize the program stats struct*/
+void initProgramStats()
+{
     program_stats.average_turnaround = 0;
     program_stats.end_time = 0;
     program_stats.total_jobs = 0;
@@ -477,15 +450,10 @@ void initProgramStats() {
     program_stats.min_turnaround = -1;
     program_stats.max_turnaround = -1;
 
-
-    struct timespec ts;
-    timespec_get(&ts, TIME_UTC);
-    program_stats.start_time = (long long int)nano_to_ms(ts.tv_nsec) + ts.tv_sec * 1000;
+    program_stats.start_time = getCurrentTime();
 }
 
-/*
- * This function validates user input, and builds the struct Program_Data
- */
+/* This function validates user input, and builds the struct Program_Data */
 bool initProgramData(int argc, char **argv)
 {
     if (argc < USER_INPUT_ARGC) {
@@ -498,7 +466,6 @@ bool initProgramData(int argc, char **argv)
     program_data->log_enable = atoi(argv[4]);
     program_data->num_of_sleeping_threads = 0;
 
-
     /* validate input */
     if (program_data->num_threads > MAX_NUM_THREADS || program_data->num_counters > MAX_NUM_COUNTERS ||
         (program_data->log_enable != 0 && program_data->log_enable != 1)) {
@@ -508,13 +475,38 @@ bool initProgramData(int argc, char **argv)
     return true;
 }
 
+/* initialize the queue struct */
 void initQueue()
 {
     JobQueue = (Queue *)malloc(sizeof(Queue));
+    if (!JobQueue) {
+        fprintf(stderr, "Memory allocation failed..\n");
+        exit(EXIT_FAILURE);
+    }
     pthread_mutex_init(&(JobQueue->queue_mutex), NULL);
     JobQueue->first_job = NULL;
     JobQueue->last_job = NULL;
     JobQueue->num_of_pending_jobs = 0;
+}
+
+/* Writes statistic data to the stats_file */
+void writeStats(FILE *stats_file)
+{
+    program_stats.average_turnaround = program_stats.total_turnaround / program_stats.total_jobs;
+
+    fprintf(stats_file, "total running time: %lld milliseconds\n",
+            (long long int)(program_stats.end_time - program_stats.start_time));
+    fprintf(stats_file, "sum of jobs turnaround time: %lld milliseconds\n",
+            (long long int)program_stats.total_turnaround);
+    fprintf(stats_file, "min job turnaround time: %lld milliseconds\n", (long long int)program_stats.min_turnaround);
+    fprintf(stats_file, "average job turnaround time: %f milliseconds\n", (double)program_stats.average_turnaround);
+    fprintf(stats_file, "max job turnaround time: %lld milliseconds\n", (long long int)program_stats.max_turnaround);
+}
+
+long long int getCurrentTime() {
+    struct timespec ts;
+    timespec_get(&ts, TIME_UTC);
+    return (long long int)nano_to_ms(ts.tv_nsec) + ts.tv_sec * 1000;
 }
 
 
@@ -524,13 +516,9 @@ int main(int argc, char **argv)
     char *line_buf = NULL;
     size_t line_buffer_size = 0;
     Job *current_job;
-    struct timespec ts;
-    
-    /* INITIALIZATIONS AND VALIDATIONS */ 
+    /* INITIALIZATIONS AND VALIDATIONS */
 
     initProgramStats();
-
-
 
     valid_args = initProgramData(argc, argv);
     if (valid_args == false) {
@@ -543,7 +531,7 @@ int main(int argc, char **argv)
         fprintf(stderr, "Error creating file: dispatcher.txt");
         exit(EXIT_FAILURE);
     }
-    FILE *stats_file = fopen("file stats.txt", "w+");
+    FILE *stats_file = fopen("file_stats.txt", "w+");
     if (!stats_file) {
         fprintf(stderr, "Error creating file: file stats.txt");
         exit(EXIT_FAILURE);
@@ -555,19 +543,19 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-
     initQueue();
     initCounters();
     initThreadsArr();
     initCounterMutexs();
 
-
     /* Loop through until we are done with the file. */
     while ((line_size = getline(&line_buf, &line_buffer_size, cmd_file)) != EOF) {
-        
-        timespec_get(&ts, TIME_UTC);
-        if(program_data->log_enable) {
-            fprintf(dispatcher_log, ": TIME %lld: read cmd line: %s", (long long int)nano_to_ms(ts.tv_nsec) + ts.tv_sec * 1000  - program_stats.start_time  , line_buf);
+
+        /* write to dispatcher log file*/
+        if (program_data->log_enable) {
+
+            fprintf(dispatcher_log, ": TIME %lld: read cmd line: %s",
+                     getCurrentTime() - program_stats.start_time, line_buf);
         }
 
         current_job = createJob(line_buf);
@@ -576,30 +564,14 @@ int main(int argc, char **argv)
 
     /* WAIT FOR PENDING JOBS TO COMPLETE*/
     if (program_data->num_of_sleeping_threads != program_data->num_threads) {
-        printf("Waiting for all jobs to complete before exiting...\n");
         waitPendingJobs();
     }
-
-
+    program_stats.end_time = getCurrentTime();
 
 
     /* WRITE STATS */
+    writeStats(stats_file);
 
-    timespec_get(&ts, TIME_UTC);
-    program_stats.end_time = (long long int)nano_to_ms(ts.tv_nsec) + ts.tv_sec * 1000;
-    program_stats.average_turnaround = program_stats.total_turnaround / program_stats.total_jobs;
-
-    fprintf(stats_file, "total running time: %lld milliseconds\n", (long long int)(program_stats.end_time - program_stats.start_time));
-    fprintf(stats_file, "sum of jobs turnaround time: %lld milliseconds\n", (long long int)program_stats.total_turnaround);
-    fprintf(stats_file, "min job turnaround time: %lld milliseconds\n", (long long int)program_stats.min_turnaround);
-    fprintf(stats_file, "average job turnaround time: %f milliseconds\n", (double)program_stats.average_turnaround);
-    fprintf(stats_file, "max job turnaround time: %lld milliseconds\n", (long long int)program_stats.max_turnaround);
-
-
-    
-
-
-    /* END OF PROGRAM PROCEDURE */
     // stop threads
     killAllThreads();
 
@@ -609,7 +581,7 @@ int main(int argc, char **argv)
     pthread_mutex_destroy(&(JobQueue->queue_mutex));
 
     // destroy file mutex'
-    for(int i = 0; i < program_data->num_counters; i++) {
+    for (int i = 0; i < program_data->num_counters; i++) {
         pthread_mutex_destroy(&(program_data->counter_mutex_arr[i]));
     }
 
